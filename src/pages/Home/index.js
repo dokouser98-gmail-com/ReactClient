99b0225475e6ccbc2f1ba562852ca93fb0ecbae6
Anthony-Ricse <dokouser98@gmail.com>
2022-02-01 20:52:31 -0300

    Fetch en productos , tranajo con proxys

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../../data.js";
+
+import Axios from "axios";
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchDatos = async () => {
+      const result = await Axios.get("api/productos");
+      setProducts(result.data);
+    };
+    fetchDatos();
+  }, []);
+
   return (
     <div>
       <div className="bg-white">
@@ -11,13 +21,13 @@ export default function Home() {
           </h2>
 
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {data.productos.map((productos) => (
-              <div key={productos.slug} className="group relative">
+            {products.productos?.map((products) => (
+              <div key={products.slug} className="group relative">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                  <Link to={`/producto/${productos.slug}`}>
+                  <Link to={`/producto/${products.slug}`}>
                     <img
-                      src={productos.image}
-                      alt={productos.nombre}
+                      src={products.image}
+                      alt={products.nombre}
                       className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                     />
                   </Link>
@@ -26,17 +36,17 @@ export default function Home() {
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <Link to={`/producto/${productos.slug}`}>
+                      <Link to={`/producto/${products.slug}`}>
                         <span aria-hidden="true" className="absolute inset-0" />
-                        {productos.nombre}
+                        {products.nombre}
                       </Link>
                     </h3>
                     <p className="mt-1 text-sm text-gray-500">
-                      {productos.categoria}
+                      {products.categoria}
                     </p>
                   </div>
                   <p className="text-sm font-medium text-gray-900">
-                    S/.{productos.precio}
+                    {products.precio}
                   </p>
                 </div>
               </div>
